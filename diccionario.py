a0f3b1b9b366a98766938525389a42533fc07fa4
ahoyter <ahoyter@gmail.com>
2014-03-24 23:10:25 +0100

    Signed-off-by: ahoyter <ahoyter@gmail.com>

---
+++ diccionario.py
@@ -0,0 +1,5 @@
+meses = {"Enero":"31","Febrero":"29","Marzo":"31"}
+print "Los meses son: ",meses
+print "Las claves son ",meses.keys()
+print "Las los campos son ",meses.values()
+print "Los dias de febrero ",meses["Febrero"]
